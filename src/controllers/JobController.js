9401aacdf185c5be1477eb5e950856a6f84e69cd
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-04-24 17:17:50 -0300

    feat: Job controller

---
+++ src/controllers/JobController.js
@@ -0,0 +1,59 @@
+const Job = require('../models/Job')
+const JobUtils = require('../utils/JobUtils')
+const Profile = require('../models/Profile')
+
+module.exports = {
+  create(req, res) {
+    return res.render('job')
+  },
+
+  async save(req, res) {
+    await Job.save({
+      name: req.body['name'],
+      'daily-hours': req.body['daily-hours'],
+      'total-hours': req.body['total-hours'],
+      'created_at': Date.now() // adicionando a data de criação
+    })
+    
+    return res.redirect('/')
+  },
+
+  async show(req, res) {
+    const jobs = await Job.get()
+    const profile = await Profile.get()
+
+    const jobId = req.params['id']
+
+    const job = jobs.find(job => job['id'] === Number(jobId)) // O job que vem pelo 'params' é string
+
+    if(!job) {
+      return res.send('Job não encontrado')
+    }
+
+    job.budget = JobUtils.calculateBudget(job, profile['value-hour'])
+
+    return res.render('job-edit', { job })
+  },
+
+  async update(req, res) {
+    const jobId = req.params['id']
+
+    const updatedJob = {
+      name: req.body['name'],
+      'total-hours': req.body['total-hours'],
+      'daily-hours': req.body['daily-hours']
+    }
+
+    await Job.update(updatedJob, jobId)
+
+    res.redirect('/job/' + jobId)
+  },
+
+  async delete(req, res) {
+    const jobId = req.params['id']
+    
+    await Job.delete(jobId)
+
+    return res.redirect('/')
+  }
+}
